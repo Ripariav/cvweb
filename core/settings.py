c47fc289a11e5556bed5b93c12a1bc045a32e830
Daniel Rivera <danielriveravelasquez@gmail.com>
2024-08-31 16:02:41 -0500

    data base manual setting

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-@8ad(qnhgtw7bksws&)8%*1!=)5$4t5hl=1q482hbd(5y7rm$r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
