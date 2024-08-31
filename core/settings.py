d564107217eb06e71368ac420379a53077dc5721
Daniel Rivera <danielriveravelasquez@gmail.com>
2024-08-31 11:56:30 -0500

    data base manual setting

--- core/settings.py
+++ core/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import psycopg2
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
